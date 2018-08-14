1dfee19d0afdeaf6f16df95cee8438f8676acd4e
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-08-14 12:14:50 +0200

    update migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 const AirDropper = artifacts.require('./AirDropper.sol');
-const ZuluToken = artifacts.require('./ZuluToken.sol');
+const ZTX = artifacts.require('./ZTX.sol');
 
 const capOnAirdropReceivers = 100000;
 
 module.exports = function(deployer) {
     return deployer
         .then(() => {
-            return deployer.deploy(ZuluToken);
+            return deployer.deploy(ZTX);
         })
         .then(() => {
             return deployer.deploy(
